0ae94afb1350e6c483eb26d23093e09552f09a7e
Kesavareddygit <122251769+Kesavareddygit@users.noreply.github.com>
2023-01-23 09:06:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -8,7 +8,7 @@ provider "aws" {
 terraform {
  backend "s3" {
    encrypt        = false
-   bucket         = "tf-bucket-s3-dev"
+   bucket         = "dev-tf-bucket-s3"
    dynamodb_table = "dev-terraform_state"
    key            = "path/path/terraform-tfstate"
    region         = "us-east-1"
