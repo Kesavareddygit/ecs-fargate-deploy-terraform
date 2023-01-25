9976673684124ba55c09ec97d07a46156bcaef97
Kesavareddygit <122251769+Kesavareddygit@users.noreply.github.com>
2023-01-25 08:40:27 +0530

    Update cognito.tf

--- cognito.tf
+++ cognito.tf
@@ -43,6 +43,6 @@ resource "aws_cognito_user_pool_client" "client" {
 }
 
 resource "aws_cognito_user_pool_domain" "cognito-domain" {
-  domain       = "reddykesava"
+  domain       = "reddykesava428"
   user_pool_id = "${aws_cognito_user_pool.user_pool.id}"
 }
